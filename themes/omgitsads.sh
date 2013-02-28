2967ccef8ddcec1dc5c346e0e12642b1412b3801
Evan Machnic <emachnic@broadmac.net>
2013-02-28 15:45:03 -0800

    Modified settings

--- themes/omgitsads.sh
+++ themes/omgitsads.sh
@@ -40,12 +40,13 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"earthquake 3 0" \
 		"pwd 89 211" \
 		"mailcount 9 255" \
+		#"ey-mailcount 9 255" \
 		"now_playing 234 37" \
 		#"cpu 240 136" \
 		"load 10" \
 		#"tmux_mem_cpu_load 234 136" \
 		"battery 12" \
-		# "weather 37 255" \
+		"weather 37 255" \
 		# "xkb_layout 125 117" \
 		# "date_day 235 136" \
 		# "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
